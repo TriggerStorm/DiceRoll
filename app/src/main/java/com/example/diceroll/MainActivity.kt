76709d391d8cdd36553de03f49c5f8cefaa10bbf
TriggerStorm <michael.h.p@hotmail.com>
2021-03-04 11:37:36 +0100

    Roll v1 working

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.diceroll
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.NumberPicker
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_history.*
+import android.widget.GridLayout
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -17,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     val diceIds = arrayOf(R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6)
 
+    var nrDice = 1
 
 
     companion object{
@@ -32,19 +32,37 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, History::class.java)
             startActivity(intent)
         }
-        numberPicker.minValue = 0
+        numberPicker.minValue = 1
         numberPicker.maxValue = 6
 
         numberPicker.wrapSelectorWheel = true
+
+        numberPicker.setOnValueChangedListener { numberPicker, oldValue, newValue -> nrDice = newValue }
+
+        val gridLayout = findViewById<GridLayout>(R.id.grid)
+
+        for (i in 0 until 6) {
+            gridLayout.addView(ImageView(this), i)
+        }
+
     }
 
     fun OnclickRoll(view: View) {
-        val d1 = mGenerator.nextInt(6)
-        val d2 = mGenerator.nextInt(6)
+       // val d1 = mGenerator.nextInt(6)
+       // val d2 = mGenerator.nextInt(6)
 
-        imgDice1.setImageResource(diceIds[d1])
-        imgDice2.setImageResource(diceIds[d2])
+       // imgDice1.setImageResource(diceIds[d1])
+      //  imgDice2.setImageResource(diceIds[d2])
+        val roll = arrayOfNulls<Int>(6)
+        for (i in 0 until nrDice) {
+            val d1 = mGenerator.nextInt(6)
+            roll[i]=d1
+        }
 
+        for (k in 0 until nrDice) {
+            val v = grid.getChildAt(k) as ImageView
+            v.setImageResource(diceIds[roll[k]!!])
+        }
 
        // rollHistory.add((d1+1 , d2+1))
         updateHistory()
